4d2554d4c2890b1a9ca7e2451f1b81ee724d30aa
unknown <micahriggan@gmail.com>
2025-02-02 21:30:37 -0500

    setting always dark mode, and windows portable

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, nativeTheme } from "electron";
 //import { createRequire } from "node:module";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
@@ -31,6 +31,8 @@ export const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
 export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
 
+nativeTheme.themeSource = 'dark';
+
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
   ? path.join(process.env.APP_ROOT, "public")
   : RENDERER_DIST;
@@ -44,6 +46,7 @@ function createWindow() {
       preload: path.join(__dirname, "preload.mjs"),
     },
   });
+
 
   // Test active push message to Renderer-process.
   win.webContents.on("did-finish-load", () => {
