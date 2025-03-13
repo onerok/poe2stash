6d145d60318cb51d5299137bf14adebcd0db53ae
unknown <micahriggan@gmail.com>
2025-03-12 20:53:18 -0400

    updating to hash router to fix some file router issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import MessagesPage from "./components/MessagesPage";
 import { SideMenu } from "./components/SideMenu";
